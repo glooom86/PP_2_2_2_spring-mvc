ec3043acd90ed64a3b005bb7414a1ad5fea3f007
glooom86 <94992697+glooom86@users.noreply.github.com>
2022-02-18 22:33:47 +0500

    Commit

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -2,13 +2,10 @@ package web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
-import web.service.CarService;
 import web.service.CarServiceImpl;
-
 import java.util.ArrayList;
 import java.util.List;
 
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,9 +1,6 @@
 package web.service;
 
-
 import web.model.Car;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class CarServiceImpl implements CarService {
